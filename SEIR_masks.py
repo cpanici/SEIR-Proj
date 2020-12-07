cc9e4dd714de2a4995f9f4368f4daffb1548e933
Christian Panici <47305647+cpanici@users.noreply.github.com>
2020-12-07 00:29:41 -0600

    Modify alpha in vaccines, add masks simulation

--- SEIR_masks.py
+++ SEIR_masks.py
@@ -8,7 +8,7 @@ GROUPS:
     E: Exposed --- NOTE: Exposed means infected, but not contagious
     I: Infected
     R: Recovered
-    V: Vaccinated
+    M: Masked
     D: Deceased
 '''
 
@@ -16,11 +16,19 @@ GROUPS:
 
 if __name__ == '__main__':
     # Population size
-    N = 10000
+    N = 100000
+
+    # Mask adoption rate. Determines how many start in S vs M
+    # Hi, avg, low: .2, .5, .9 for each
+    mask_adoption = .5
+    mask_effectiveness = .5
 
     # P(S -> E)
-    r0 = 1.2        # avg number of people an infected person will spread to, scale by pop to get "per person" avg
+    r0 = 1.2
     alpha = r0/N
+
+    #P(M -> E)
+    alpha_m = alpha*(1-mask_effectiveness)
 
     # P(E -> I) -- set as .375 because it usually takes 2-4 days to become infectious?
     # should think ab this a bit more
@@ -33,28 +41,26 @@ if __name__ == '__main__':
     recovery_rate = .97  # somewhere between [.97, .9975]
     death_rate = 1 - recovery_rate
 
-    # went with this value because it is unknown how many people can be vaccinated per day, but it would likely be at least
-    # 1% of population
-    num_vaccinated_per_day = N * .01
-
 
     days = []
+    deaths = []
     for i in range(100):
 
         # for debugging
-        N_list = []
+        N_list = [N]
 
-        S = [N-1]
+        S = [N*(1-mask_adoption)-1]
+        M = [N*mask_adoption]
         E = [0]
         I = [1]
         R = [0]
         D = [0]
-        V = [0]
         t = 0
 
         # Run until all recovered or we run out of infected/exposed people
         while (I[t] > 0 or E[t] > 0):
             S.append(binomial(S[t], (1 - alpha) ** I[t]))
+            M.append(binomial(M[t], (1 - alpha_m)**I[t]))
 
             # since the path from I could go to R or D, we first calculate the num that go to R or D
             num_leaving_I = binomial(I[t], beta)
@@ -68,37 +74,13 @@ if __name__ == '__main__':
             I.append(binomial(E[t], exposed_rate))
 
 
-
-            # calculating vaccination rate
-            # if t == 250:
-            #     susceptible_at_t250 = S[t]  # number of people vaccinated is function of this val
-            #
-            #     # assuming new vaccinations are fixed
-            #     # see google doc for justifications for .002
-            #     num_vaccinated_per_day = math.floor(.002 * susceptible_at_t250)
-
-            # moving from S -> V
-            if t < 250:
-                V.append(0)
-
-            else:
-                # edge case if remainder of susceptible population gets vaccinated
-                if S[t] <= num_vaccinated_per_day:
-                    V.append(V[t] + S[t])
-                    S[t + 1] = 0
-
-                else:
-                    V.append(V[t] + num_vaccinated_per_day)
-                    S[t + 1] = S[t + 1] - num_vaccinated_per_day
-
-
             # NOTE: formula for E is what the formula for I was under the SIR model
-            E.append(N - S[t + 1] - I[t + 1] - R[t + 1] - D[t + 1] - V[t + 1])
+            E.append(N - S[t + 1] - I[t + 1] - R[t + 1] - D[t + 1] - M[t + 1])
 
             # adjusting
 
             # need to make sure S + R + D + I + V == N
-            N_list.append(S[t] + E[t] + R[t] + D[t] + I[t] + V[t])
+            N_list.append(S[t] + E[t] + R[t] + D[t] + I[t] + M[t])
 
 
 
@@ -106,32 +88,18 @@ if __name__ == '__main__':
 
 
         days.append(t)
+        deaths.append(D[-1])
 
-    print(sum(days) / len(days))
+    print(sum(days) / len(days), 'avg days')
+    print(sum(deaths)/len(deaths), 'avg deaths')
 
     plt.figure(figsize=(15, 5))
     plt.plot(range(0, t + 1), S, color='red', label='Susceptible')
     plt.plot(range(0, t + 1), E, color='green', label='Exposed')
     plt.plot(range(0, t + 1), I, color='blue', label='Infected')
     plt.plot(range(0, t + 1), R, color='orange', label='Recovered')
-    plt.plot(range(0, t + 1), V, color='purple', label='Vaccinated')
+    plt.plot(range(0, t + 1), M, color='purple', label='Masked')
     plt.plot(range(0, t + 1), D, color='black', label='Deceased')
+    plt.plot(range(0, t+1), N_list, color='pink', label='Total')
     plt.legend()
     plt.show()
-
-# q=1
-# print(N_list[q])
-# print(S[q])
-# print(E[q])
-# print(I[q])
-# print(R[q])
-# print(D[q])
-# print(V[q])
-
-# print(N_list[q]-N_list[q-1])
-# print(S[q] - S[q-1])
-# print(E[q] - E[q-1])
-# print(I[q] - I[q-1])
-# print(R[q] - R[q-1])
-# print(D[q] - D[q-1])
-# print(V[q] - V[q-1])
